8a2d0aca81b250596405fb55e4d3a226ede567a1
Akrm Sid Ahmed <asid-ahm@lab1r4s5.42abudhabi.ae>
2024-07-13 15:52:36 +0400

    found error

--- BSRC/join_space.c
+++ BSRC/join_space.c
@@ -6,12 +6,11 @@
 /*   By: asid-ahm <asid-ahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 17:03:07 by asid-ahm          #+#    #+#             */
-/*   Updated: 2024/07/13 15:45:59 by asid-ahm         ###   ########.fr       */
+/*   Updated: 2024/07/13 15:51:28 by asid-ahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-// #include <stdlib.h>
 
 #include <string.h>
 #include <stdlib.h>
@@ -19,12 +18,11 @@
 int ft_inside_quotes(t_inside *inside , int c){
 	if(c == '\'' && !inside->dquotes){
         inside->quotes = !inside->quotes;
-    }else if(c == '\"' && !inside->quotes){
-			inside->dquotes = !inside->quotes;
+    }else if(c == '"' && !inside->quotes){
+			inside->dquotes = !inside->dquotes;
 		}
 		return (1);
 }
-
 static int	is_special(char c)
 {
 	if (c == '<' || c == '>' || c == '|')
@@ -39,9 +37,14 @@ char *joined_str(char *line)
 	int		i;
 	int		n; /// name it better
 	char	c;
+	t_inside *inside;
+
+	inside = malloc(sizeof (t_inside));
+	inside->dquotes = 0;
+	inside->quotes = 0;
 
 	i = 0;
-	size = strlen(line);
+	size = ft_strlen(line);
 	while(line[i])
 	{
 		c = line[i];
@@ -57,8 +60,9 @@ char *joined_str(char *line)
 	while (line[i])
 	{
 		c = line[i];
-		if (is_special(c) && (ft_inside_quotes == 1)) /////this not working
+		ft_inside_quotes(inside , c);
 		{
+		if (is_special(c) && !(inside->dquotes || inside->quotes))
 			result[n++] = ' ';
 			c = line[i];
 			if (is_special(c))
